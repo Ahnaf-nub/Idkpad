69bc17b1f34ceb646e1b884bddb782c6084f9fe4
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2025-03-02 13:26:03 +0600

    Add files via upload

--- rules.mk
+++ rules.mk
@@ -5,10 +5,4 @@ OLED_DRIVER = ssd1306
 DIRECT_KEY_ENABLE = yes
 
 ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
 LTO_ENABLE = yes
-
-RGBLIGHT_ENABLE = yes
-WS2812_DRIVER = vendor
-RGB_MATRIX_ENABLE = no  # Disable RGB Matrix if not used
-RGBLIGHT_ANIMATIONS = yes  # Enable lighting animations
